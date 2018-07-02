09f8e5f1b1bcc92d65a0708289500828b4def051
korulis <broliukas@gmail.com>
2018-07-02 18:45:47 +0300

    initial commit

---
+++ web-api/src/Pizza.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+order = {"address": "S. Staneviciaus 14-28", "name": "Justinas"}
+
+
+@app.route("/")
+def GetAll():
+    m1 = "This application is called: " + __name__
+    m1 = jsonify(order)
+    return m1
+
+@app.route("/test", methods=["POST"])
+def AddOne():
+    return "testas"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return name
+
+print("sth default");
+print(__name__);
+
+###! /usr/bin/python3
+###print("Hello, world!")
