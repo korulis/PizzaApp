84f6a040cb866794d947e14b13f757667013796e
korulis <broliukas@gmail.com>
2018-07-03 21:36:26 +0300

    py mongo test code

--- web-api/src/Pizza.py
+++ web-api/src/Pizza.py
@@ -5,12 +5,23 @@ from pprint import pprint
 app = Flask(__name__)
 
 order = {"address": "S. Staneviciaus 14-28", "name": "Justinas"}
-
+MongoClient
 
 @app.route("/")
 def GetAll():
     m1 = "This application is called: " + __name__
     m1 = jsonify(order)
+
+    client = MongoClient('localhost', 27017)
+    db = client['test-database']
+    collection = db['test-collection']
+    orderId = collection.insert_one(order).inserted_id
+    db.collection_names(include_system_collections=False)
+#    my_collections = db.collection_names(include_system_collections=False)
+    first_doc =collection.find_one()
+    pprint(first_doc)
+
+    m1 = "This application is called: " + "   "
     return m1
 
 @app.route("/test/<id>", methods=["POST"])
