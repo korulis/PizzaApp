47ca6f6e62e9fd2b2795a823b702f0b1d139f54c
korulis <broliukas@gmail.com>
2018-07-04 22:16:09 +0300

    initial getAllOrders implementation

--- web-api/src/Pizza.py
+++ web-api/src/Pizza.py
@@ -1,11 +1,40 @@
-from bson.json_util import dumps
+from bson.json_util import dumps,loads
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from pprint import pprint
+from src.Messenger import Messenger
+import uuid
 
 app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client['test-database']
+collection = db['test-collection']
+
+@app.route("/orders/")
+def GetAllOrders():
+    all_docs = [remove_key(x, "_id") for x in list(collection.find())]
+    all_docs = list(collection.find({}, {'_id': False}))
+    print(all_docs)
+    print(len(all_docs))
+    serialized = dumps(all_docs)
+    aaa = loads(serialized)
+    lstmano = list(aaa)
+    print(lstmano)
+    #uzu = jsonify(lstmano)
+    #return jsonify({"afds":"sadfas"})
+    return jsonify(all_docs)
+
+
+
+def remove_key(d, key):
+    r = dict(d)
+    del r[key]
+    return r
+
 order = {"address": "S. Staneviciaus 14-28", "name": "Justinas"}
+order2 = Messenger(address = "Stanevicius", name = "Justinas")
+order2.orderRef = 1
 
 @app.route("/")
 def GetAll():
@@ -13,10 +42,12 @@ def GetAll():
     m1 = "This application is called: " + __name__
     #    m1 = jsonify(order)
 
-    client = MongoClient('localhost', 27017)
-    db = client['test-database']
-    collection = db['test-collection']
-    orderId = collection.insert_one(order).inserted_id
+    print("afds")
+    jj = dumps(order2.getDict())
+    print(jj)
+    print("afdsdsa")
+
+    orderId = collection.insert_one(order2.getDict()).inserted_id
     db.collection_names(include_system_collections=False)
     #    my_collections = db.collection_names(include_system_collections=False)
     first_doc = list(collection.find())
