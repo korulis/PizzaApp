4d597da6fbdc5d9c01611270e3d81515e6cee3f0
korulis <broliukas@gmail.com>
2018-07-04 22:45:27 +0300

    Create one implementation with duplicate check

--- web-api/src/Pizza.py
+++ web-api/src/Pizza.py
@@ -13,54 +13,33 @@ collection = db['test-collection']
 
 @app.route("/orders/")
 def GetAllOrders():
-    all_docs = [remove_key(x, "_id") for x in list(collection.find())]
     all_docs = list(collection.find({}, {'_id': False}))
     print(all_docs)
-    print(len(all_docs))
-    serialized = dumps(all_docs)
-    aaa = loads(serialized)
-    lstmano = list(aaa)
-    print(lstmano)
-    #uzu = jsonify(lstmano)
-    #return jsonify({"afds":"sadfas"})
     return jsonify(all_docs)
 
 
 
-def remove_key(d, key):
-    r = dict(d)
-    del r[key]
-    return r
 
-order = {"address": "S. Staneviciaus 14-28", "name": "Justinas"}
 order2 = Messenger(address = "Stanevicius", name = "Justinas")
 order2.orderRef = 1
 
-@app.route("/")
-def GetAll():
-    print("starter")
-    m1 = "This application is called: " + __name__
-    #    m1 = jsonify(order)
+@app.route("/orders/<orderRef>", methods=["POST"])
+def CreateOne(orderRef):
+    order = request.json
+    order["orderRef"] = orderRef
+    duplicate = collection.find_one({"orderRef": orderRef})
 
-    print("afds")
-    jj = dumps(order2.getDict())
-    print(jj)
-    print("afdsdsa")
+    if(duplicate is not None):
+        return jsonify({}), 409
 
-    orderId = collection.insert_one(order2.getDict()).inserted_id
-    db.collection_names(include_system_collections=False)
-    #    my_collections = db.collection_names(include_system_collections=False)
-    first_doc = list(collection.find())
-    serialized = dumps(first_doc)
-    #pprint(first_doc)
-    print(serialized)
-    m1 = "This application is called: " + "   "
-    return m1
+    orderId = collection.insert_one(order).inserted_id
+    return jsonify({"orderRef": orderRef}), 201
 
 @app.route("/test/<id>", methods=["POST"])
 def AddOne(id):
-    content = request.json
-    print(content)
+    order = request.json
+
+    print(order)
     print(id)
     return "testas"
 
